5fa8592fd3d1457305cec3bbcc511db9291d2d50
anhthuan95 <thuan.hust.95@gmail.com>
2019-11-11 16:39:09 +0700

    tao xong api

--- src/main/java/com/codegym/lastproject/LastProjectApplication.java
+++ src/main/java/com/codegym/lastproject/LastProjectApplication.java
@@ -1,13 +1,57 @@
 package com.codegym.lastproject;
 
+import com.codegym.lastproject.formatter.RoleFormatter;
+import com.codegym.lastproject.service.RoleService;
+import com.codegym.lastproject.service.UserService;
+import com.codegym.lastproject.service.impl.RoleServiceImpl;
+import com.codegym.lastproject.service.impl.UserServiceImpl;
+import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.format.Formatter;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LastProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LastProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LastProjectApplication.class, args);
+    }
+
+    @Bean
+    public UserService userService() {
+        return new UserServiceImpl();
+    }
+
+    @Bean
+    public RoleService roleService() {
+        return new RoleServiceImpl();
+    }
+
+    @Configuration
+    @EnableSpringDataWebSupport
+    class WebConfig implements WebMvcConfigurer, ApplicationContextAware {
+
+        private ApplicationContext appContext;
+
+        @Override
+        public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+            appContext = applicationContext;
+        }
+
+        @Override
+        public void addFormatters(FormatterRegistry registry) {
+            RoleService roleService = appContext.getBean(RoleService.class);
+            Formatter roleFormatter = new RoleFormatter(roleService);
+            registry.addFormatter(roleFormatter);
+        }
+    }
+
 
 }
--- src/main/java/com/codegym/lastproject/model/User.java
+++ src/main/java/com/codegym/lastproject/model/User.java
@@ -21,4 +21,81 @@ public class User {
     @ManyToOne
     @JoinColumn(name = "role_id")
     private Role role;
+
+    public User() {
+    }
+
+    public User(String email, String password, String name, String phone, String address, String avatar, Role role) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+        this.avatar = avatar;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
